7a65cd89e50ab16d071bb0e696458acc9fc73c76
kpnishanth <nishanth.prabhakaran@gmail.com>
2017-10-25 22:43:07 -0500

    test6

--- test.java
+++ test.java
@@ -3,7 +3,7 @@ public class test{
 public static void main (String args[]){
 
 System.out.println("Hello Nishanth");
-System.out.println("Sum of two numbers is 2389");
+System.out.println("Sum of two numbers is 26767");
 }
 
 
